4aa4d09b5d40f4140887fc77c128db3f5eaab564
LairLiu <liukunair@gmail.com>
2020-05-09 14:24:53 +0800

    增加open，防止游戏停止时也执行按钮操作

--- Tetris/src/KeyBorad.ts
+++ Tetris/src/KeyBorad.ts
@@ -2,6 +2,7 @@ class KeyBorad {
   constructor() {
 
   }
+  static open: boolean = true;
   static isDebug: boolean = false;
   static onLeft: () => void
   static onRight: () => void
@@ -11,27 +12,28 @@ class KeyBorad {
 
   static initControl() {
     window.onkeydown = (ev: KeyboardEvent) => {
+      if (!this.open) return;
       this.isDebug && console.log('userclick', ev.key, 'keycode=', ev.keyCode);
       switch (ev.keyCode) {
         case 32:    //空格
           this.isDebug && console.log('空格');
-          this.onSpace();
+          this.onSpace && this.onSpace();
           break;
         case 87:    // W、上
           this.isDebug && console.log('上');
-          this.onUp();
+          this.onUp && this.onUp();
           break;
         case 83:    // S、下
           this.isDebug && console.log('下');
-          this.onDown();
+          this.onDown && this.onDown();
           break;
         case 65:    // A、左
           this.isDebug && console.log('左');
-          this.onLeft();
+          this.onLeft && this.onLeft();
           break;
         case 68:    // D、右
           this.isDebug && console.log('右');
-          this.onRight();
+          this.onRight && this.onRight();
           break;
       }
     }
