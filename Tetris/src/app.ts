ff54e14cba0fcc0135c455da2139f8d63014dcb0
LairLiu <liukunair@gmail.com>
2020-05-09 14:31:14 +0800

    添加外部对游戏内的操作与监听

--- Tetris/src/app.ts
+++ Tetris/src/app.ts
@@ -1,18 +1,37 @@
 import Game from "./Game";
-import KB from "./KeyBorad";
 
-window.onload = function () {
+const _parent = document.getElementById("game") as HTMLDivElement;
+const _scene = document.getElementById("scene") as HTMLDivElement;
+const _next = document.getElementById("next") as HTMLDivElement;
+const _score = document.getElementById("score") as HTMLDivElement;
+const _maxscore = document.getElementById("maxscore") as HTMLDivElement;
+const _pause = document.getElementById("pause") as HTMLDivElement;
+const _restart = document.getElementById("restart") as HTMLDivElement;
+
+
+let game = new Game(_scene, _next);
+
+game.onGameStart = maxscore => {
+  _score.innerText = "0";
+  _maxscore.innerText = maxscore.toString();
 }
-const aprent = document.getElementById("game") as HTMLDivElement;
-const scene = document.getElementById("scene") as HTMLDivElement;
-const next = document.getElementById("next") as HTMLDivElement;
 
-let game = new Game(scene, next);
-// console.table(game.sceneData);
+game.onGameOver = score => {
+  console.log("app", "gameover", score);
+}
 
-KB.isDebug = false; KB.initControl();
-KB.onRight = () => { game.right(); }
-KB.onLeft = () => { game.left(); }
-KB.onDown = () => { game.down(); }
-KB.onUp = () => { game.rotate(); }
+game.onScoreUpdate = (score, maxscore) => {
+  _score.innerText = score.toString();
+  _maxscore.innerText = maxscore.toString();
+}
 
+game.startGame();
+
+_pause.onclick = () => {
+  game.pause();
+  _pause.innerText = game.isPaused ? "resume" : "pause";
+}
+
+_restart.onclick = () => {
+  game.reStartGame();
+}
